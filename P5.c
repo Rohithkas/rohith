ceb587a3096e49aaa99771256091b8a490bda707
Rohithkas <49344261+Rohithkas@users.noreply.github.com>
2019-04-06 23:43:11 +0530

    Delete P5.c

--- P5.c
+++
@@ -1,7 +0,0 @@
-int main()
-{
-    int i =0;
-    printf("\n\tEnter How Many Number of Threads You Want : ");
-    scanf("%d",&sizeoft);
-    pthread_t thread[sizeoft];
-	printf("\n\t##          Creating %d Threads              ##",sizeoft);
